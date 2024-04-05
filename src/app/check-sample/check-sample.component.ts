c3bf19dabcda52c7e0863678a6548a5469909aee
evertonfranca1992 <everton.wambaster@hotmail.com>
2024-04-05 16:40:50 -0300

    Atualizações

--- src/app/check-sample/check-sample.component.ts
+++ src/app/check-sample/check-sample.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	DoCheck,
+	AfterContentChecked,
+	AfterContentInit,
+	AfterViewChecked,
+	AfterViewInit,
+	OnDestroy
+ } from '@angular/core';
 
 @Component({
   selector: 'app-check-sample',
   templateUrl: './check-sample.component.html',
   styleUrls: ['./check-sample.component.css']
 })
-export class CheckSampleComponent implements OnInit {
+export class CheckSampleComponent
+implements OnInit,
+           DoCheck,
+           AfterContentChecked,
+           AfterContentInit,
+           AfterViewChecked,
+           AfterViewInit,
+					 OnDestroy
+{
+
+	quantidade:number = 0
 
   constructor() { }
+	ngOnDestroy(): void {
+		console.log('goodbye my friend');
+	}
 
-  ngOnInit(): void {
+	adicionar(){
+		this.quantidade += 1
+	}
+
+	decrementar(){
+		this.quantidade -= 1
+	}
+
+	ngAfterViewInit(): void {
+		console.log('ngAfterViewInit');
+	}
+	ngAfterViewChecked(): void {
+		console.log('ngAfterViewChecked');
+	}
+	ngAfterContentInit(): void {
+		console.log('ngAfterContentInit');
+	}
+	ngAfterContentChecked(): void {
+		console.log("ngAfterContentChecked");
+	}
+  ngDoCheck(): void {
+		console.log("ngDoCheck")
+  }
+	ngOnInit(): void {
+		console.log("ngOnInit")
   }
 
 }
